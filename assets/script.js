2849e3fd6bf85a43678776c1a460763d60c4ea73
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-02-21 22:03:10 -0500

    move jquery and day.js to body instead of header

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,9 @@
 // Get the current time and date using Day.js
 const currentTime = () => {
-    return dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
-  };
-  
-  // Update the current time every second
-  setInterval(() => {
-    document.getElementById("current-time").textContent = currentTime();
-  }, 1000);
-  
+  return dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
+};
+
+// Update the current time every second
+setInterval(() => {
+  document.getElementById("current-time").textContent = currentTime();
+}, 1000);
